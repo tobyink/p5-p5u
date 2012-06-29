2829a7c0acd368e90945edc22db969a716fc3e32
Toby Inkster <mail@tobyinkster.co.uk>
2012-06-29 17:58:23 +0100

    test::pod tool

---
+++ lib/P5U/Lib/TestPod.pm
@@ -0,0 +1,43 @@
+package P5U::Lib::TestPod;
+
+use 5.010;
+use strict;
+use utf8;
+
+BEGIN {
+	$P5U::Command::TestPod::AUTHORITY = 'cpan:TOBYINK';
+	$P5U::Command::TestPod::VERSION   = '0.001';
+};
+
+use Path::Class;
+use Path::Class::Rule;
+use Test::More;
+use Test::Pod;
+
+sub uniq
+{
+	my %already;
+	grep { not $already{"$_"}++ } @_;
+}
+
+sub test_pod
+{
+	my $self = shift;
+	
+	my @files = 
+		uniq
+		map {
+			(-d $_)
+				? Path::Class::Rule::->new->or(
+					Path::Class::Rule::->new->perl_module,
+					Path::Class::Rule::->new->perl_pod,
+					Path::Class::Rule::->new->perl_script,
+					)->all($_)
+				: Path::Class::File::->new($_)
+		} @_;
+	
+	plan tests => scalar @files;
+	pod_file_ok("$_", $_) for @files;
+}
+
+1;
